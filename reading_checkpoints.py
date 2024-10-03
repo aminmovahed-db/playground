faf95e68baaa1ca7c3fa76b392b2d4eb02f838f9
Amin Movahed <amin.movahed@databricks.com>
2024-10-03 20:11:30 +1000

    Updating the script

--- reading_checkpoints.py
+++ reading_checkpoints.py
@@ -5,5 +5,5 @@ max_commit = spark.read.json('dbfs:/tmp/nab_shared_mode_test/commits').selectExp
 df = (spark.read
       .json('dbfs:/tmp/nab_shared_mode_test/offsets')
       .where("reservoirId is not null")
-      .where(f"_metadata.file_name = '{max_commit}'")).withColumn("Version", col("reservoirVersion") + col("index")).selectExpr('reservoirId', 'Version')
+      .where(f"_metadata.file_name = '{max_commit}'")).withColumn("Version", col("reservoirVersion") + col("index")).selectExpr('reservoirId', 'Version','_metadata.file_name').withColumnRenamed('_metadata.file_name', 'commit_version')
 display(df)

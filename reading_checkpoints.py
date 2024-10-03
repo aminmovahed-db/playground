f31aecd9d2f5dcf643de8b6b2d02bf89fa98c0e4
Amin Movahed <amin.movahed@databricks.com>
2024-10-03 20:04:15 +1000

    Adding script to extract the source versions from checkpoints

---
+++ reading_checkpoints.py
@@ -0,0 +1,9 @@
+from pyspark.sql.functions import max, col
+
+max_commit = spark.read.json('dbfs:/tmp/nab_shared_mode_test/commits').selectExpr('_metadata.file_name').agg(max('_metadata.file_name')).collect()[0][0]
+
+df = (spark.read
+      .json('dbfs:/tmp/nab_shared_mode_test/offsets')
+      .where("reservoirId is not null")
+      .where(f"_metadata.file_name = '{max_commit}'")).withColumn("Version", col("reservoirVersion") + col("index")).selectExpr('reservoirId', 'Version')
+display(df)
